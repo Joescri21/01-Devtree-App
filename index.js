7b9f92802e04e1546a771489d1be894678f5577d
Jool Escribano <joescri21@gmail.com>
2025-08-12 16:09:34 -0600

    Devtree-App

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hola mundo desde Devtree')
+//CJS Common JS const express = require ('express');  
+import express from 'express'; // ESM Ecmascript modules
+
+const app = express ();
+
+// Routing 
+app.get('/', (req, res) => {
+    res.send('Hola mundo desde Express');
+});
+
+// Server o Port
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+    console.log('Servidor Funcionando en el puerto...', port);
+})
